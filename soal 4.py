978ed203fac15caae3aaff7a743a16dfa77b30bb
sunnyegg <53990968+sunnyegg@users.noreply.github.com>
2019-09-07 22:11:22 +0700

    soal 4.py
    yeeyy

--- soal 4.py
+++ soal 4.py
@@ -5,6 +5,14 @@ data = "993141 -1 1323 14-232"
 apus1 = re.sub('-','',data)
 apus2 = re.split('\s',apus1)
 gabung1 = ''.join(apus2)
-gabung2 = re.findall('.{1,3}',gabung1)
-gabung3 = '-'.join(gabung2)
-print(gabung3)
+pisah = re.findall('.{1,4}',gabung1)
+pisah[0:3] = [''.join(pisah[0:3])]
+gabung2 = re.findall('.{3}',pisah[0])
+gabung3 = re.findall('.{2}',pisah[1])
+gabung4 = '-'.join(gabung2)+'-'
+gabung5 = '-'.join(gabung3)
+gabung6 = gabung4+gabung5
+gabung7 = ''.join(gabung6)
+print(gabung7)
+
+#puyeng :(
